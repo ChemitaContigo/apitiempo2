a1dd35bfc9d46fe8d469b74aa67b75cb1faa9fa8
Vicente <vsolis272@gmail.com>
2023-09-07 17:16:10 +0200

    Initial commit

---
+++ src/main/java/com/example/apitiempo/ApitiempoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitiempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitiempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitiempoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apitiempo/ApitiempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitiempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitiempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
